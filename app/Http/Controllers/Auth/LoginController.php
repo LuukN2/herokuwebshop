1f407bb96cd49cc8b778701a4d6e8756cdc05d91
Unknown <lw.naaijen@avans.nl>
2018-04-01 19:07:09 +0200

    reeee
    re

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -24,7 +24,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/products';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
