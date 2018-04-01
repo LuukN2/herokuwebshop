4867bbfe022a58e3cc5b3ae32b31608e19ece75f
Unknown <rgemert1@student.avans.nl>
2018-04-01 20:24:38 +0200

    About/Home

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -10,7 +10,15 @@
 {!! Breadcrumbs::render('about') !!}
 <div class="container">
     <div class="whitebackground">
-    
+        <h1>About</h1>
+        <p>Bordspellen en puzzels bestaat uit een super enthausiast team van twee 
+            jonge developers. Wij hebben beide een passie voor bordspellen en puzzels
+            daarom hebben wij besloten deze webshop te beginnen.<br> Onze eerste founder
+            is Luuk, Luuk heeft al zijn hele leven een passie voor bordspellen en met
+            name het spel Kolonisten van Catan.<br> Onze andere founder is Ryan, Ryan puzzelt
+            er al jaren lustig op los en geniet ook van een rustig avondje kaarten.
+        
+        </p>
     </div>
 </div>
  
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,15 @@
 {!! Breadcrumbs::render('home') !!}
 <div class="container">
     <div class="whitebackground">
-    
+        <h1>Welkom!</h1>
+        <p>Welkom op onze bordspellen en puzzels webshop.<br><br>
+            Bij ons staat de kwaliteit van het product voorop samen met de klant.
+            Wij van bordspellen en puzzels vinden een avond met een bordspel of een puzzel
+            een essentieel deel van het leven. Al ontzettend lang groeien kinderen op met deze 
+            spellen en puzzels daarom willen wij de ideale ervaring bieden bij het bestelling van deze dingen.
+            Ook zijn we er natuurlijk voor de ouderen die van een rustige puzzel middag houden of een wild spelletje
+            risk.
+        </p>
     </div>
 </div>
  
