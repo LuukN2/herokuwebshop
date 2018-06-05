97495ad13b7da1486e08361eb7c5801b9cc392bd
Unknown <lw.naaijen@avans.nl>
2018-06-05 23:39:36 +0200

    asdasd

--- public/js/code.js
+++ public/js/code.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     $("#productSearch").on("input", function () {
            var input, filter; 
             input = $('#productSearch'); 
-            filter = $('#productSearch').attr('value'); 
+            filter = $('#productSearch').attr('value').toLowerCase(); 
     $(".col-sm-6.col-md-4.product").each(function (index, value) { 
         var title = $(this).find(".title"); 
         var categories = $(this).find(".category"); 
