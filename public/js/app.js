9eb4ae85ae966e0971d4365be8ee1eecd149f9ec
Unknown <lw.naaijen@avans.nl>
2018-06-05 21:40:32 +0200

    Auto stash before merge of "master" and "origin/master"

--- public/js/app.js
+++ public/js/app.js
@@ -13366,3 +13366,38 @@
         }]
     }
 }, function (t, e) {}]);
+    
+    
+    
+    
+    $(document).ready(function () { 
+   
+    $(document).on("keypress", ".productSearch", function (e) { 
+           var input, filter; 
+            input = document.getElementById('productSearch'); 
+            filter = input.value.toLowerCase(); 
+ 
+    $(".col-sm-6.col-md-4.product").each(function () { 
+        var title = $(this).find(".title"); 
+        var categories = $(this).find(".category"); 
+        var subs = $(this).find(".sub"); 
+        if (~title.text().toLowerCase().indexOf(filter)) 
+            $(this).css("display", "block"); 
+        else { 
+            var matched = false; 
+            categories.each(function () { 
+               if(~$(this).text().toLowerCase().indexOf(filter)) 
+                   matched = true; 
+            }); 
+            subs.each(function () { 
+               if(~$(this).text().toLowerCase.indexOf(filter)) 
+                   matched = true; 
+            }); 
+            if(!matched) 
+                $(this).css("display", "none"); 
+        } 
+             
+    });  
+    }); 
+     
+}); 
