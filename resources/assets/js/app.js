57473da65e181683d7917982d18412381115e7c4
Unknown <lw.naaijen@avans.nl>
2018-06-05 21:26:58 +0200

    sadasd

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -21,35 +21,34 @@ const app = new Vue({
     el: '#app'
 });
 
-$(document).ready(function () {
-    $(document).on("type", ".productSearch", function (e) {
-           var input, filter;
-            input = document.getElementById('productSearch');
-            filter = input.value.toLowerCase();
-
-    $(".col-sm-6.col-md-4.product").each(function () {
-        var title = $(this).find(".title");
-        var categories = $(this).find(".category");
-        var subs = $(this).find(".sub");
-        if (~title.text().toLowerCase().indexOf(filter))
-            $(this).css("display", "block");
-        else {
-            var matched = false;
-            categories.each(function () {
-               if(~$(this).text().toLowerCase().indexOf(filter))
-                   matched = true;
-            });
-            subs.each(function () {
-               if(~$(this).text().toLowerCase.indexOf(filter))
-                   matched = true;
-            });
-            if(!matched)
-                $(this).css("display", "none");
-        }
-            
+$(document).ready(function () { 
+    $(document).on("type", ".productSearch", function (e) { 
+           var input, filter; 
+            input = document.getElementById('productSearch'); 
+            filter = input.value.toLowerCase(); 
+ 
+    $(".col-sm-6.col-md-4.product").each(function () { 
+        var title = $(this).find(".title"); 
+        var categories = $(this).find(".category"); 
+        var subs = $(this).find(".sub"); 
+        if (~title.text().toLowerCase().indexOf(filter)) 
+            $(this).css("display", "block"); 
+        else { 
+            var matched = false; 
+            categories.each(function () { 
+               if(~$(this).text().toLowerCase().indexOf(filter)) 
+                   matched = true; 
+            }); 
+            subs.each(function () { 
+               if(~$(this).text().toLowerCase.indexOf(filter)) 
+                   matched = true; 
+            }); 
+            if(!matched) 
+                $(this).css("display", "none"); 
+        } 
+             
+    });  
     }); 
-    });
-    
-});
-
-
+     
+}); 
+ 
