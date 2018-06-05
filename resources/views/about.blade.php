f620e5739dded31e00760b9b4f325d0099507949
Unknown <rgemert1@student.avans.nl>
2018-06-05 20:03:04 +0200

    Test

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -15,7 +15,7 @@
             jonge developers. Wij hebben beide een passie voor bordspellen en puzzels
             daarom hebben wij besloten deze webshop te beginnen.<br> Onze eerste founder
             is Luuk, Luuk heeft al zijn hele leven een passie voor bordspellen en met
-            name het spel Kolonisten van Catan.<br> Onze andere founder is Ryan, Ryan puzzelt
+            name het spel Kolonisten van Catan en Stratego.<br> Onze andere founder is Ryan, Ryan puzzelt
             er al jaren lustig op los en geniet ook van een rustig avondje kaarten.
         
         </p>
