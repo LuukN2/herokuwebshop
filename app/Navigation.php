55c28b2f078b228ea66bc14b87c505c223ebd32c
Unknown <lw.naaijen@avans.nl>
2018-06-06 15:54:01 +0200

    asdasd
    a

--- app/Navigation.php
+++ app/Navigation.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Navigation extends Model
 {
+    public $timestamps = false;
      protected $table = 'navigations';
     protected $fillable = ['name','url','admin','id'];
 
