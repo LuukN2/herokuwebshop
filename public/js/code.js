4db2936242168ef34bafafed01330e07df92a7fe
Unknown <lw.naaijen@avans.nl>
2018-06-06 13:38:16 +0200

    asdasd

--- public/js/code.js
+++ public/js/code.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
 
         if (~title.text().toLowerCase().indexOf(filter)) 
             $(this).css("display", "block"); 
-        
+            alert(title.text());
         else { 
             var matched = false; 
             categories.each(function () { 
